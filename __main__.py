5fa83245704418f48bb24a37ed67b9d9da963fd4
Muhammad <tjmnkrajyej@gmail.com>
2024-10-06 02:18:36 +0100

    Import bt from `__main__.py` as a package

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import importlib
 import os.path as path
 import sys
 
-spec = importlib.util.spec_from_file_location("bt", path.join(path.dirname(path.realpath(__file__)), "__init__.py"))
+spec = importlib.machinery.PathFinder().find_spec("bt", [path.dirname(path.dirname(path.realpath(__file__)))])
 bt = importlib.util.module_from_spec(spec)
 bt.MAIN = 1
 sys.modules[bt.__name__] = bt
